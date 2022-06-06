f406226ab812832edb45312ee01ad7a93fba995a
shakhbozbekusmonov <miracle_0319@bk.ru>
2022-06-06 17:11:31 +0500

    Created Project

---
+++ README.old.md
@@ -0,0 +1,2 @@
+# app-employee-client
+This is React Hook and Formik used Employee App!
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './sass/main.scss';
+import {Employee} from "./components/Employee";
+
+function App() {
+  return (
+    <>
+      <Employee/>
+    </>
+  );
+}
+
+export default App;
